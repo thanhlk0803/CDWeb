f7d81b8c261636064a98be1b63280f02b6d77034
thanhlk0803 <20211TT1281@mail.tdc.edu.vn>
2022-12-10 14:37:08 +0700

    fix banner slider

--- galaxy-film/src/component/Slider.js
+++ galaxy-film/src/component/Slider.js
@@ -3,26 +3,13 @@ import React from 'react';
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import BookTickets from "../component/BookTickets";
-import {Carousel} from "react-bootstrap";
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { GetListFilm } from '../config';
-
-function MyComponent ()  {
-    const [ListSlider, setList] = useState([]);
-  useEffect(() => {
-    GetListFilm()
-      .then((data) => {
-        setList(data);
-      
-      })
-      .catch((err) => console.log("error =>", err));
-  }, []);
-console.log(ListSlider);
+import { Carousel } from "react-bootstrap";
+import '../css/flim.css';
+const MyComponent = () => {
     return (
         <div >
-            <Carousel fade>
-                
+
+            <Carousel>
                 <Carousel.Item>
                     <img
                      src={'https://cdn.galaxycine.vn/media/2022/11/25/khaitruongnhatrang-digital-2048x682_1669346464818.jpg'}
